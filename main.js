532c989fde8400695b73ca45babeb360f4704348
benjamin <louis.benjamin@live.fr>
2024-11-27 15:47:01 +0100

    6 premieres lessons

--- main.js
+++ main.js
@@ -1,6 +1,39 @@
 var app = new Vue({
     el:'#app',
     data: {
-        product : "Chaussettes"
+        product : "Chaussettes",
+        description : "Une paire de chaussettes chaudes et confortables",
+        image:'./images/vmSocks-green-onWhite.jpg',
+        altText:"Une paire de chaussettes vertes",
+        inStock:true,
+        inventory: 0,
+        onSale: false,
+        details: ["80% coton", "20% polyester","Unisexe"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green",
+                variantImage: "./images/vmSocks-green-onWhite.jpg"
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue",
+                variantImage: "./images/vmSocks-blue-onWhite.jpg"
+            }
+        ],
+        tailles: ["35-39","40-42","43-45"],
+        cart: 0
+    },
+    methods:{
+       addToCart: function (){
+        this.cart+=1
+       },
+       updateProduct: function(variantImage){
+        this.image = variantImage
+       },
+       deleteFromCart: function (){
+        if (this.cart>0){
+        this.cart-=1}
+       }
     }
 })
